c1071245ca1f656124ccdb651093cf6190b223d5
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-13 20:53:44 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -87,7 +87,11 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
-		super.create();
+		#if android
+		addVirtualPad(UP_DOWN, A_B_C);
+		#end	
+		
+			super.create();
 	}
 
 	override function closeSubState() {
